b6ec27992b4ac5ae68fc5b92af96e1cafe746440
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-08-01 12:05:02 +0100

    1636. Sort Array by Increasing Frequency - python - v1

---
+++ 1636. Sort Array by Increasing Frequency/python/v1.py
@@ -0,0 +1,8 @@
+from typing import Counter, List
+
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        nums.sort(key=lambda n: (count[n], -n))
+        return nums
