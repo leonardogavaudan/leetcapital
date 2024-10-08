6e3e1a93daba378a90eb64adde4a17bb6809c9a4
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-10-08 15:44:27 +0200

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves - python - v2

---
+++ 1509. Minimum Difference Between Largest and Smallest Value in Three Moves/python/v2.py
@@ -0,0 +1,17 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        if len(nums) <= 4:
+            return 0
+
+        min_four = sorted(heapq.nsmallest(4, nums))
+        max_four = sorted(heapq.nlargest(4, nums))
+        res = float("inf")
+
+        for i in range(4):
+            res = min(res, max_four[i] - min_four[i])
+
+        return int(res)
