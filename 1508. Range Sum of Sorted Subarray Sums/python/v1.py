1887d4c7b2d3f9e328ec40f9f9432ae5d0c4cb70
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-10-18 11:55:08 +0100

    1508. Range Sum of Sorted Subarray Sums - python - v1

---
+++ 1508. Range Sum of Sorted Subarray Sums/python/v1.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        sums = []
+
+        for i in range(len(nums)):
+            total = 0
+            for j in range(i, len(nums)):
+                total += nums[j]
+                sums.append(total)
+
+        sums.sort()
+
+        return sum(sums[left - 1 : right]) % (10**9 + 7)
