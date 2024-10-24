ccd47b597be41abe420c65a4f97b210c66130d29
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-10-24 16:32:50 +0200

    786. K-th Smallest Prime Fraction - python - v1 - remove unused hash      table

--- 786. K-th Smallest Prime Fraction/python/v1.py
+++ 786. K-th Smallest Prime Fraction/python/v1.py
@@ -4,11 +4,9 @@ from typing import List
 
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        table = {i: len(arr) - 1 for i in range(len(arr) - 1)}
-
-        heap = []
-        for start, end in table.items():
-            heapq.heappush(heap, (arr[start] / arr[end], start, end))
+        heap = [
+            (arr[i] / arr[len(arr) - 1], i, len(arr) - 1) for i in range(len(arr) - 1)
+        ]
 
         for _ in range(k - 1):
             _, start, end = heapq.heappop(heap)
