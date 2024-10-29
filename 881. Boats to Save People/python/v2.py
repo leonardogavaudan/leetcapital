bf22ab1af44972366eabaf90e8ba0141549352b2
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-10-29 16:29:42 +0100

    881. Boats to Save People - python - v1

---
+++ 881. Boats to Save People/python/v2.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        l, r = 0, len(people) - 1
+        res = 0
+
+        people.sort()
+
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                l += 1
+
+            r -= 1
+            res += 1
+
+        return res
