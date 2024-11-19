c07905f64395d55b006cc22c8934dbde8bd60236
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-19 17:53:14 +0100

    small corrections

--- 1140. Stone Game II/python/v3.py
+++ 1140. Stone Game II/python/v3.py
@@ -21,18 +21,18 @@ class Solution:
                 piles_count = 0
                 for j in range(1, 2 * m + 1):
                     piles_count += piles[i + j - 1]
-                    min_max_result = min_max(i + j, max(m, 2 * j), False)
+                    min_max_result = min_max(i + j, max(m, j), False)
                     max_res = max(max_res, piles_count + min_max_result)
                 memo[(i, m, is_alice_turn)] = max_res
                 return max_res
             else:
                 if 2 * m + i - 1 >= len(piles) - 1:
                     return 0
-                min_res = 2**15 - 1
+                min_res = float("inf")
                 for j in range(1, 2 * m + 1):
-                    min_max_result = min_max(i + j, max(m, 2 * j), True)
+                    min_max_result = min_max(i + j, max(m, j), True)
                     min_res = min(min_res, min_max_result)
                 memo[(i, m, is_alice_turn)] = min_res
                 return min_res
 
-        return min_max(0, 1, True)
+        return int(min_max(0, 1, True))
