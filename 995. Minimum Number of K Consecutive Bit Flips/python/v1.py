8c64cc249f3551abb8f01b6990c9b2b87752a526
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-25 18:37:44 +0100

    style change to prev problem

--- 995. Minimum Number of K Consecutive Bit Flips/python/v1.py
+++ 995. Minimum Number of K Consecutive Bit Flips/python/v1.py
@@ -5,14 +5,13 @@ class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
         res = 0
         current_flips = 0
-        n = len(nums)
 
-        for i in range(n):
+        for i in range(len(nums)):
             if i >= k and nums[i - k] == 2:
                 current_flips -= 1
 
             if (nums[i] + current_flips) % 2 == 0:
-                if i + k > n:
+                if i + k - 1 >= len(nums):
                     return -1
                 res += 1
                 current_flips += 1
