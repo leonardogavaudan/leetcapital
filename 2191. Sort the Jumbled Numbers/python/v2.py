df111c92687434bea5879ae7fde07b92369bf4f9
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-27 15:09:42 +0100

    2191. Sort the Jumbled Numbers - python - v2

---
+++ 2191. Sort the Jumbled Numbers/python/v2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        to_mapping = {i: value for i, value in enumerate(mapping)}
+        def map_to_new(num: int) -> int:
+            nonlocal to_mapping
+            if num == 0:
+                return to_mapping[0]
+
+            res = 0
+            i = 0
+            while num:
+                res += to_mapping[(num % 10)] * 10**i
+                num //= 10
+                i +=1
+            return res
+
+        return list(map(lambda x: nums[x[1]], sorted([(map_to_new(value), i) for i, value in enumerate(nums)])))
