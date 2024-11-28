257226522642e2f45d3c22bf3116eb02b20d489d
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-28 16:37:54 +0100

    921. Minimum Add to Make Parentheses Valid - python - v1

---
+++ 921. Minimum Add to Make Parentheses Valid/python/v1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        res = 1
+        opening_count = 0
+        for c in s:
+            if c == "(":
+                opening_count += 1
+            if c == ")":
+                if opening_count:
+                    opening_count -= 1
+                else:
+                    res += 1
+
+        return res + opening_count
