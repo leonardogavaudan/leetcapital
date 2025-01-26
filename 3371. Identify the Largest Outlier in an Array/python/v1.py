cadfa44442fce9a77274b284f4410d2cac32a9d4
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-01-26 19:15:31 +0100

    3371. Identify the Largest Outlier in an Array - python - v1

---
+++ 3371. Identify the Largest Outlier in an Array/python/v1.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        total = sum(nums)
+        nums.sort()
+
+        for i in range(len(nums) - 1, -1, -1):
+            total_without_special = total - nums[i]
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                target = total_without_special - nums[mid]
+                if nums[mid] == target:
+                    if mid != i:
+                        return nums[i]
+                    if i - 1 > -1 and nums[i - 1] == nums[i]:
+                        return nums[i]
+                    break
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        raise AssertionError("Solution not found")
