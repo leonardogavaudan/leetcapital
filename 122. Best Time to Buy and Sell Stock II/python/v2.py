68434ca8c46f023193122d3a33763ba5e005bd73
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-03-10 10:37:04 +0100

    122. Best Time to Buy and Sell Stock II - python - v2

---
+++ 122. Best Time to Buy and Sell Stock II/python/v2.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        for i in range(len(prices) - 1):
+            res += max(0, prices[i + 1] - prices[i])
+
+        return res
