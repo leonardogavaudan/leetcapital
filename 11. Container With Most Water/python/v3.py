50378ca6b49aaf3f107b63d33ca07e61e5bd99ef
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-03-12 16:19:00 +0100

    11. Container With Most Water - python - v3

---
+++ 11. Container With Most Water/python/v3.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        left, right = 0, len(height) - 1
+
+        while left < right:
+            new_area = (right - left) * min(height[left], height[right])
+            res = max(res, new_area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return res
